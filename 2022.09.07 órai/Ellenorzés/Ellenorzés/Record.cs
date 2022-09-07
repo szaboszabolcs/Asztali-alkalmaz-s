487041a6efd26fb3af595f34bafbac886b8a42cb
szaboszabolcs <93977842+szaboszabolcs@users.noreply.github.com>
2022-09-07 11:40:09 +0200

    órai közös feladat

---
+++ 2022.09.07 órai/Ellenorzés/Ellenorzés/Record.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ellenorzés
+{
+    internal class Record
+    {
+        public string rendszam;
+        public int bOra;
+        public int bPerc;
+        public int bMp;
+        public int bEmp;
+        public int kOra;
+        public int kPerc;
+        public int kMp;
+        public int kEmp;
+
+        public Record(string sor)
+        {
+            string[] Mezok = sor.Split(' ');
+            this.rendszam = Mezok[0];
+            this.bOra = int.Parse(Mezok[1]);
+            this.bPerc = int.Parse(Mezok[2]);
+            this.bMp = int.Parse(Mezok[3]);
+            this.bEmp = int.Parse(Mezok[4]);
+            this.kOra = int.Parse(Mezok[5]);
+            this.kPerc = int.Parse(Mezok[6]);
+            this.kMp = int.Parse(Mezok[7]);
+            this.kEmp = int.Parse(Mezok[8]);
+        }
+    }
+}
---
+++ 2022.09.07 órai/Ellenorzés/Ellenorzés/ellenorzes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Ellenorzés
+{
+    internal class ellenorzes
+    {
+        static void Main(string[] args)
+        {
+            List<Record> records = new List<Record>();
+            StreamReader reader = new StreamReader("meresek.txt");
+            while (!reader.EndOfStream)
+            {
+                Record egyRecord= new Record(reader.ReadLine());
+                records.Add(egyRecord);
+
+            }
+            reader.Close();
+            Console.WriteLine("2. feladat");
+            Console.WriteLine($"A mérés során {records.Count} jármű adatait rögzítették.");
+            Console.WriteLine("3. feladat");
+            int db = 0;
+            foreach (Record egyRecord in records)
+            {
+                if (egyRecord.kOra<9)
+                {
+                    db++;
+                }
+            }
+            Console.WriteLine($"9 óra előtt {db} jármű haladt el a végponti mérőnél.");
+            Console.ReadKey();
+        }
+    }
+}
